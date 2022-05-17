a7cae4dcc9243ef0a3d97549a5cc0fa2920219b7
rafAmorim8 <rafael_amorim30@hotmail.com>
2022-05-16 21:28:05 -0700

    creates Header and Summaru

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
+import { Dashboard } from "./components/Dashboard";
+import { Header } from "./components/Header"
 import { GlobalStyle } from "./styles/global"
 
 export function App() {
   return (
-    <div className="App">
+    <>
       <GlobalStyle />
+      <Header />
+      <Dashboard />
       <h1>dtmoney</h1>
-    </div>
+    </>
   );
 }
